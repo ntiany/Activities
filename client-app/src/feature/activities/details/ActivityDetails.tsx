f801f0451d7d24d2321b714d8bdef9973d90ca94
ntiany <krolkas@gmail.com>
2020-06-17 08:14:32 +0200

    Add info to activity cards

--- client-app/src/feature/activities/details/ActivityDetails.tsx
+++ client-app/src/feature/activities/details/ActivityDetails.tsx
@@ -27,7 +27,7 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({match, hi
         <Grid>
             <Grid.Column width={10}>
                 <ActivityDetailsHeader activity={activity} />
-                <ActivityDetailsInfo />
+                <ActivityDetailsInfo activity={activity}/>
                 <ActivityDetailsChat/>
             </Grid.Column>
             <Grid.Column width={6}>
