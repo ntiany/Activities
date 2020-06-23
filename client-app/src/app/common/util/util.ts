8d4951af64e1e24f07d310eaa916b6e010da13f9
ntiany <krolkas@gmail.com>
2020-06-23 10:10:17 +0200

    Combine date and time in create form

---
+++ client-app/src/app/common/util/util.ts
@@ -0,0 +1,10 @@
+﻿export const combinedDateAndTime = (date: Date, time: Date) => {
+    const timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDay();
+    const dateString = `${year}-${month}-${day}`;
+
+    return new Date(dateString + ' ' + timeString);
+}
