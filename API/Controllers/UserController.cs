dbd18ad307babcaad1be09ec66824e71d3669644
ntiany <krolkas@gmail.com>
2020-06-24 15:49:32 +0200

    Add user registration

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -9,10 +9,16 @@ namespace API.Controllers
     [AllowAnonymous]
     public class UserController : BaseController
     {
-        [HttpPost]
+        [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(Register.Command command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
---
+++ Application/User/Register.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interface;
+using Domain;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.User
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+
+            public string UserName { get; set; }
+            
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.UserName).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _dataContext;
+
+            private readonly UserManager<AppUser> _userManager;
+
+            private readonly IJwtGenerator _jwtGenerator;
+
+            public Handler(DataContext dataContext, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            {
+                _dataContext = dataContext;
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (await _dataContext.Users.AnyAsync(x => x.Email == request.Email))
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new {Email = "Email already exists"});
+                }
+
+                if (await _dataContext.Users.AnyAsync(x => x.UserName == request.UserName))
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new { UserName = "Username already exists" });
+                }
+
+                var user = new AppUser()
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.UserName,
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+
+                if (result.Succeeded)
+                {
+                    return new User 
+                    {
+                        DisplayName = request.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        Username = request.UserName,
+                        Image = null
+                    };
+                }
+
+                throw new Exception("Problem creating user");
+            }
+        }
+    }
+}
