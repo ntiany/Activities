36de4a18e1a3b868875fcbcf8ffb8620992ccfc7
ntiany <krolkas@gmail.com>
2020-06-24 16:26:57 +0200

    Add controller option to get current user

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -3,22 +3,32 @@ using Application.User;
 using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
+    
     public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Register.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<User>> CurrentUser()
+        {
+            return await Mediator.Send(new CurrentUser.Query());
         }
     }
 }
