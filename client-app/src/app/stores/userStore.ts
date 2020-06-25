d3a7fa44da67f5927af8db498b08aa042d7015c2
ntiany <krolkas@gmail.com>
2020-06-25 16:17:44 +0200

    Add register form

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -20,9 +20,21 @@ export default class UserStore {
             runInAction(() => this.user = user
             );
             this.rootStore.commonStore.setToken(user.token);
+            this.rootStore.modalStore.closeModal();
             history.push('/activities');
         } catch (error) {
            throw error;
+        }
+    }
+
+    @action register = async (values : IUserFormValues) => {
+        try {
+            const user = await agent.User.register(values);
+            this.rootStore.commonStore.setToken(user.token);
+            this.rootStore.modalStore.closeModal();
+            history.push('/activities');
+        } catch (error) {
+            error.log(error);
         }
     }
 
