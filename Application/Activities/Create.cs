213b9bc9a40e585100cc62107d9ea88ce0b778b2
ntiany <krolkas@gmail.com>
2020-06-25 19:01:25 +0200

    Add current user when creating new activity

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
+using Application.Interface;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -40,14 +42,17 @@ namespace Application.Activities
                 RuleFor(x => x.Venue).NotEmpty();
             }
         }
-
+         
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
 
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
@@ -64,6 +69,20 @@ namespace Application.Activities
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivity.Add(attendee);
+
                 var success = await _context.SaveChangesAsync(cancellationToken) > 0;
 
                 if (success)
