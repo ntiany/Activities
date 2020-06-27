1b27ed2057154328e72f326000800447f20713bb
ntiany <krolkas@gmail.com>
2020-06-27 15:27:23 +0200

    Add profile header component

--- client-app/src/feature/profiles/ProfilePage.tsx
+++ client-app/src/feature/profiles/ProfilePage.tsx
@@ -1,8 +1,15 @@
 ﻿import React from 'react'
 import { observer } from 'mobx-react-lite'
+import { Grid } from 'semantic-ui-react';
+import ProfileHeader from './ProfileHeader';
 
 const ProfilePage = () => {
-    return (<div>Profile page</div>
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader/>
+            </Grid.Column>
+        </Grid>
     );
 }
 
