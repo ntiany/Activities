9107350cd865d04362993e9e5284b17ab0fbbf3d
ntiany <krolkas@gmail.com>
2020-06-27 10:20:02 +0200

    Add controller to handle adding pictures

---
+++ API/Controllers/PhotosController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using Application.Photos;
+using Domain;
+
+namespace API.Controllers
+{
+    public class PhotosController : BaseController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Photo>> Add([FromForm]Add.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
