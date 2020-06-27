72df717b9956a35b350cb11cbea4713a98e5fd4f
ntiany <krolkas@gmail.com>
2020-06-27 15:35:00 +0200

    Add profile content component

---
+++ client-app/src/feature/profiles/ProfileContent.tsx
@@ -0,0 +1,53 @@
+﻿import React from 'react'
+import { observer } from 'mobx-react-lite'
+import { Grid, Tab } from 'semantic-ui-react';
+
+const panes = [
+    {
+        menuItem: 'About',
+        render: () =>
+            <Tab.Pane>
+                About content
+            </Tab.Pane>
+    },
+    {
+    menuItem: 'Photos',
+        render: () => 
+        <Tab.Pane>
+            About content
+        </Tab.Pane>
+    },
+    {
+        menuItem: 'Activities',
+        render: () =>
+            <Tab.Pane>
+                About content
+            </Tab.Pane>
+    },
+    {
+        menuItem: 'Followers',
+        render: () =>
+            <Tab.Pane>
+                About content
+            </Tab.Pane>
+    },
+    {
+        menuItem: 'Following',
+        render: () =>
+            <Tab.Pane>
+                About content
+            </Tab.Pane>
+    }
+];
+
+const ProfileContent = () => {
+    return (
+        <Tab
+            menu={{ fluid: true, vertical: true }}
+            menuPosition='right'
+            panes={panes}
+        />
+    );
+}
+
+export default observer(ProfileContent)
--- client-app/src/feature/profiles/ProfilePage.tsx
+++ client-app/src/feature/profiles/ProfilePage.tsx
@@ -2,12 +2,14 @@
 import { observer } from 'mobx-react-lite'
 import { Grid } from 'semantic-ui-react';
 import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
 
 const ProfilePage = () => {
     return (
         <Grid>
             <Grid.Column width={16}>
-                <ProfileHeader/>
+                <ProfileHeader />
+                <ProfileContent />
             </Grid.Column>
         </Grid>
     );
