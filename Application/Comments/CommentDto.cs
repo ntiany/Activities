24763a88173b6f3d63bd3663ea371c8385de5061
ntiany <krolkas@gmail.com>
2020-06-29 16:24:47 +0200

    Add comments dto

---
+++ Application/Comments/CommentDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+
+namespace Application.Comments
+{
+    public class CommentDto
+    {
+        public Guid Id { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Username { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Image { get; set; }
+    }
+}
---
+++ Application/Comments/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+namespace Application.Comments
+{
+    public class MappingProfile : AutoMapper.Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Comment, CommentDto>()
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+        }
+    }
+}
