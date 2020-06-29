a9df3df277e579d25695d328bc863dc68927be2c
ntiany <krolkas@gmail.com>
2020-06-29 19:08:28 +0200

    Add groups to signalR

--- API/SignalR/ChatHub.cs
+++ API/SignalR/ChatHub.cs
@@ -25,7 +25,30 @@ namespace API.SignalR
 
             var comment = await _mediator.Send(command);
 
-            await Clients.All.SendAsync("ReceiveComment", comment);
+            await Clients.Group(command.ActivityId.ToString()).SendAsync("ReceiveComment", comment);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var username = GetUsername();
+
+            await Clients.Group(groupName).SendAsync("Send", $"${username} has joined the group");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            var username = GetUsername();
+
+            await Clients.Group(groupName).SendAsync("Send", $"{username} has left the group");
+        }
+
+        private string GetUsername()
+        {
+            return Context.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
         }
     }
 }
