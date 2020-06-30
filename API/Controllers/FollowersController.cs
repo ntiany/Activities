459b43678ef7b705d0218b0037ac4b9271c47b22
ntiany <krolkas@gmail.com>
2020-06-30 10:38:21 +0200

    Add listing followers

--- API/Controllers/FollowersController.cs
+++ API/Controllers/FollowersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Application.Followers;
 using Application.Profiles;
 using MediatR;
@@ -19,6 +20,12 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Unfollow(string username)
         {
             return await Mediator.Send(new Delete.Command { Username = username });
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowings(string username, string predicate)
+        {
+            return await Mediator.Send(new List.Query {Username = username, Predicate = predicate});
         }
     }
 }
