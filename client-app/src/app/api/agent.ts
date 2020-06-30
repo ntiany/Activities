8d4b9325039dfa2fe11a706fcf3c055f45f612cd
ntiany <krolkas@gmail.com>
2020-06-30 19:09:18 +0200

    Remove sleep from axios methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -48,32 +48,23 @@ axios.interceptors.response.use(undefined, error => {
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) =>
-    new Promise<AxiosResponse>(resolve =>
-        setTimeout(() => resolve(response), ms)
-    );
-
 const requests = {
     get: (url: string) =>
         axios
             .get(url)
-            .then(sleep(1000))
-            .then(responseBody),
+        .then(responseBody),
     post: (url: string, body: {}) =>
         axios
             .post(url, body)
-            .then(sleep(1000))
-            .then(responseBody),
+        .then(responseBody),
     put: (url: string, body: {}) =>
         axios
             .put(url, body)
-            .then(sleep(1000))
-            .then(responseBody),
+        .then(responseBody),
     del: (url: string) =>
         axios
             .delete(url)
-            .then(sleep(1000))
-            .then(responseBody),
+        .then(responseBody),
     postForm: (url: string, file: Blob) => {
         let formData = new FormData();
         formData.append('File', file);
@@ -87,7 +78,7 @@ const requests = {
 
 const Activities = {
     list: (params: URLSearchParams): Promise<IActivitiesEnvelope> =>
-        axios.get('/activities', { params: params }).then(sleep(1000)).then(responseBody),
+        axios.get('/activities', { params: params }).then(responseBody),
     details: (id: string) => requests.get(`/activities/${id}`),
     create: (activity: IActivity) => requests.post('/activities', activity),
     update: (activity: IActivity) =>
